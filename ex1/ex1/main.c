98999a769299091bcb393b7be6c97968edd68065
Shanti <remixn95@gmail.com>
2013-02-26 14:25:18 +0100

    shanti-final

--- ex1/ex1/main.c
+++ ex1/ex1/main.c
@@ -1,18 +1,26 @@
 //
 //  main.c
 //  ex1
-//
+//  Shanti
 //  Created by Shanti on 26/02/13.
 //  Copyright (c) 2013 Shanti. All rights reserved.
-//
+//  1. Introduir dos nombres per teclat. Dissenyar una funció que retorni el més gran dels dos nombres entrats.
 
 #include <stdio.h>
+int main()
 
-int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    int num1;
+    int num2;
+    
+    printf("introducir el primer numero: ");
+    scanf("%d",&num1);
+    
+    printf("introducir segundo numero: ");
+    scanf("%d",&num2);
+    
+    
     return 0;
 }
+
 
