ee233d5de88add8100ebef3aa55aa9a409011e01
Shanti <remixn95@gmail.com>
2013-02-26 14:11:54 +0100

    shanti

---
+++ ex1/ex1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ex1
+//
+//  Created by Shanti on 26/02/13.
+//  Copyright (c) 2013 Shanti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
