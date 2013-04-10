36084c8909aa5dfd0a40a1dee3d13bbb219f2e64
Shanti <remixn95@gmail.com>
2013-04-10 21:42:27 +0200

    act 4

---
+++ actividad 4/act4/act4/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  act4
+//
+//  Created by Shanti on 10/04/13.
+//  Copyright (c) 2013 Shanti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
